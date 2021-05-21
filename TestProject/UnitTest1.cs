fbdf0f2a3a1c83fef032fcd9613f6215c3656582
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-21 10:31:31 +0530

    [sailesh] Add: STP1-Calculate Fare

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+using cabinvoiceGenerator;
+using NUnit.Framework;
+
+namespace TestProject
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator = null;
+        [SetUp]
+        public void Setup()
+        {
+        }
+        
+
+    }
+
+
+}
